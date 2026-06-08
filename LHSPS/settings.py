10c6251e050a6d5e027bdf4aaf6968581acc1ba0
lookhere <your@email.com>
2026-06-08 10:31:48 +0800

    Fix Cloudinary config: add CLOUDINARY_STORAGE dict for django-cloudinary-storage

--- LHSPS/settings.py
+++ LHSPS/settings.py
@@ -152,6 +152,11 @@ if CLOUDINARY_CLOUD_NAME and CLOUDINARY_API_KEY and CLOUDINARY_API_SECRET:
         secure=True,
     )
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
+        'API_KEY': CLOUDINARY_API_KEY,
+        'API_SECRET': CLOUDINARY_API_SECRET,
+    }
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
